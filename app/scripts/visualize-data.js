cd539f48e8e053b904402bf058b99b489b43b1fd
Marcin Skirzynski <marcin.skirzynski@gmail.com>
2014-10-26 21:57:36 -0400

    Move through history of iterations

--- app/scripts/visualize-data.js
+++ app/scripts/visualize-data.js
@@ -1,5 +1,5 @@
 /* global d3, Perceptron */
-/* exported generateData, learn, rerenderPerceptronFunction  */
+/* exported generateData, learn */
 
 // Using an global array as the container
 // for all added nodes for now.
@@ -39,7 +39,7 @@ var updateErrorStatus = function(errors) {
 // and the according d3 data set will be updated
 // with information about the guess, answer and error.
 // Also, an update and redrawing of d3 will be triggered.
-var applyAndUpdate = function() {
+var applyAndUpdate = function(p) {
   // apply and ...
   var errors = 0;
   for(var i=0; i<nodes.length; i++) {
@@ -83,9 +83,8 @@ var learnAll = function() {
 
     p.train([x, y], answer);
   }
-  rerenderPerceptronFunction();
-  applyAndUpdate();
-  incrementAndUpdateIterations();
+  rerenderPerceptronFunction(p);
+  applyAndUpdate(p);
 };
 
 var learnStepByStep = function(callback) {
@@ -113,14 +112,13 @@ var learnStepByStep = function(callback) {
     node.wrong  = (guess!==answer);
 
     // Updating
-    rerenderPerceptronFunction();
+    rerenderPerceptronFunction(p);
     updateErrorStatus(countErrors());
     nodeSVGroup.selectAll(".node")
         .data(nodes)
         .attr("class", updateNodeClass);
     if(++i === nodes.length) {
       clearInterval(stepTimer);
-      incrementAndUpdateIterations();
       if(typeof callback === "function") {
         callback.call();
       }
@@ -129,6 +127,7 @@ var learnStepByStep = function(callback) {
   },timePerNode);
 };
 
+var iterations = [];
 // Read the options and call the learn function accordingly
 var learn = function() {
   flawlessAll();
@@ -137,11 +136,29 @@ var learn = function() {
 // Learn all the data at once until we have no more errors
 var flawlessAll = function() {
   var timer = setInterval(function(){
+    iterations.push(p.clone());
     learnAll();
     if(countErrors() === 0) {
+      iterations.push(p.clone());
       clearInterval(timer);
+      $("#sliders .iterations span.value").html(iterations.length -1); 
+      $("#sliders").slideDown(1000);
+      $(function() {
+        $("#sliders .iterations > div.slider").slider({
+          min: 0,
+          max: iterations.length - 1,
+          value: iterations.length - 1,
+          slide: function(event, ui) { 
+            var i           = ui.value;
+            var currentPerp = iterations[i];
+            $("#sliders .iterations span.value").html(i); 
+            rerenderPerceptronFunction(currentPerp);
+            applyAndUpdate(currentPerp);
+          }
+        });
+      });
     }
-  }, 1000);
+  }, 300);
 };
 
 // Learn all the data stey by step until we have no more errors
@@ -153,20 +170,14 @@ var flawlessStepByStep = function() {
   }
 };
 
-var iterations = 0;
-var incrementAndUpdateIterations = function() {
-  iterations++;
-  $('div#iterations span#counter').html(iterations);
-};
-
-var updatePercpFunction = function() {
+var updatePercpFunction = function(p) {
   $("#percp-m").val(p.m().toFixed(3));
   $("#percp-b").val(p.b().toFixed(3));
 };
 
 // Updates the learned function in the
 // d3.js visualization and displays it.
-var rerenderPerceptronFunction = function() {
+var rerenderPerceptronFunction = function(p) {
   linesSVGroup.selectAll("#pline")
     .data([{"x1":-1, "y1": p.f(-1), "x2":4, "y2": p.f(4)}])
     .transition().duration(1000).ease('elastic')
@@ -174,7 +185,7 @@ var rerenderPerceptronFunction = function() {
     .attr("y1", function(d) { return d.y1; })
     .attr("x2", function(d) { return d.x2; })
     .attr("y2", function(d) { return d.y2; });
-  updatePercpFunction();
+  updatePercpFunction(p);
 };
 
 // Updates the function which we wanna learn in the
@@ -201,13 +212,13 @@ var generateData = function() {
     var y = Math.random() * 3;
     addNode(x, y);
   }
-  applyAndUpdate();
+  applyAndUpdate(p);
 };
 
 // Adds a node and directly applies as well as updates d3.
 var addAndApplyNode = function(x, y) {
   addNode(x, y);
-  applyAndUpdate();
+  applyAndUpdate(p);
 };
 
 // d3.js initialization stuff
@@ -216,8 +227,8 @@ var width = 300,
 
 // The "real" function we want to learn
 var real = {
-  m : 1,
-  b : 0,
+  m : 0.1,
+  b : 1,
   f : function(x) { return this.m * x + this.b; }
 };
 
@@ -243,7 +254,7 @@ var realAnswer = function(x, y) {
 // Input size is two since we want to visualize the data.
 var p = new Perceptron(2);
 
-updatePercpFunction();
+updatePercpFunction(p);
 
 var svg = d3.select("div#data .canvas").append("svg")
               .attr("width", width)
@@ -282,20 +293,3 @@ linesSVGroup.selectAll("#pline")
     .attr("y2", function(d) { return d.y2; })
     .attr("id", "pline")
     .attr("class", "line");
-
-$(function() {
-  $("#sliders .iterations > div.slider").slider({
-    min: 0,
-    max: 21,
-    value: 1,
-    slide: function(event, ui) { $("#sliders .iterations span.value").html(ui.value); }
-  });
-});
-$(function() {
-  $("#sliders .points > div.slider").slider({
-    min: 0,
-    max: 100,
-    value: 1,
-    slide: function(event, ui) { $("#sliders .points span.value").html(ui.value); }
-  });
-});

d6134bc1cd95d8d085896eaaa13b229a2151ab1d
Marcin Skirzynski <marcin.skirzynski@gmail.com>
2014-10-26 22:19:00 -0400

    Applying some best practice

--- app/scripts/visualize-percp.js
+++ app/scripts/visualize-percp.js
@@ -1,67 +1,69 @@
 /*global d3, Perceptron */
 
-var width = 300,
-    height = 200;
+(function(d3, Perceptron) {
+  var width = 300,
+      height = 200;
 
-var color = d3.scale.category20();
+  var color = d3.scale.category20();
 
-var force = d3.layout.force()
-    .charge(-200)
-    .linkDistance(80)
-    .size([width, height]);
+  var force = d3.layout.force()
+      .charge(-200)
+      .linkDistance(80)
+      .size([width, height]);
 
-var svg = d3.select("div#perceptron").append("svg")
-    .attr("width", width)
-    .attr("height", height);
+  var svg = d3.select("div#perceptron").append("svg")
+      .attr("width", width)
+      .attr("height", height);
 
-var p = new Perceptron(2);
-var nodes = p.graph().nodes;
-var links = p.graph().links;
+  var p = new Perceptron(2);
+  var nodes = p.graph().nodes;
+  var links = p.graph().links;
 
-var mnode = {fixed: true, r: 0};
-nodes.push(mnode);
+  var mouse = {fixed: true, r: 0};
+  nodes.push(mouse);
 
-force
-    .nodes(nodes)
-    .links(links)
-    .start();
+  force
+      .nodes(nodes)
+      .links(links)
+      .start();
 
-var link = svg.selectAll(".link")
-    .data(links)
-  .enter().append("line")
-    .attr("class", "link")
-    .style("stroke-width", function(d) { return d.weight; });
+  var link = svg.selectAll(".link")
+      .data(links)
+    .enter().append("line")
+      .attr("class", "link")
+      .style("stroke-width", function(d) { return d.weight; });
 
-var node = svg.selectAll(".node")
-    .data(nodes)
-  .enter().append("g")
-    .call(force.drag);
- 
-    node.append("circle")
-    .attr("class", "node")
-    .attr("r", function(d) { if(d.r===0) {return 0;} else {return d.group === 2 ? 25 : 15;}})
-    .style("fill", function(d) { return color(d.group); });
+  var node = svg.selectAll(".node")
+      .data(nodes)
+    .enter().append("g")
+      .call(force.drag);
+   
+  node.append("circle")
+      .attr("class", "node")
+      .attr("r", function(d) { if(d.r===0) {return 0;} else {return d.group === 2 ? 25 : 15;}})
+      .style("fill", function(d) { return color(d.group); });
 
-    node.append("text")
-    .attr("x", -5)
-    .attr("y", 5)
-    .text(function(d) { return d.label; });
+  node.append("text")
+      .attr("x", -5)
+      .attr("y", 5)
+      .text(function(d) { return d.label; });
 
-svg.on("mousemove", function() {
-  var p1 = d3.mouse(this);
-  mnode.px = p1[0];
-  mnode.py = p1[1];
-  force.resume();
-});
+  svg.on("mousemove", function() {
+    var p1 = d3.mouse(this);
+    mouse.px = p1[0];
+    mouse.py = p1[1];
+    force.resume();
+  });
 
-node.append("title")
-    .text(function(d) { return d.name; });
+  node.append("title")
+      .text(function(d) { return d.name; });
 
-force.on("tick", function() {
-  link.attr("x1", function(d) { return d.source.x; })
-      .attr("y1", function(d) { return d.source.y; })
-      .attr("x2", function(d) { return d.target.x; })
-      .attr("y2", function(d) { return d.target.y; });
+  force.on("tick", function() {
+    link.attr("x1", function(d) { return d.source.x; })
+        .attr("y1", function(d) { return d.source.y; })
+        .attr("x2", function(d) { return d.target.x; })
+        .attr("y2", function(d) { return d.target.y; });
 
-  node.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
-});
+    node.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
+  });
+}(d3, Perceptron));
